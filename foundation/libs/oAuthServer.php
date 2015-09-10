939fe89e36563a618bcbab8065bf0134719e0295
gollomer <gollomer@gmail.com>
2015-09-10 09:58:52 -0700

    Using PhalconPHP PDO connection and using users table instead of oauth_users

--- foundation/libs/oAuthServer.php
+++ foundation/libs/oAuthServer.php
@@ -31,26 +31,23 @@
  * Appropriate Legal Notices must display the words "Powered by Projects4Me".
  */
 
+/**
+ * @todo Change the AutoLoader and create a new storage for Phalcon
+ * @todo Load the user_table from config or from the Users model
+ */
 
-$dsn      = 'mysql:dbname=projects4me;host=localhost';
-$username = 'root';
-$password = '123qwe';
-
-// error reporting (this is a demo, after all!)
-ini_set('display_errors',1);error_reporting(E_ALL);
-
-// Autoloading (composer is preferred, but for this example let's just do this)
 require_once(APP_PATH.'/vendors/OAuth2/Autoloader.php');
 OAuth2\Autoloader::register();
 
-// $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
-$storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
+$pdo = \Phalcon\Di::getDefault()->get('db')->getInternalHandler();
+
+$storage = new OAuth2\Storage\Pdo($pdo,array('user_table'=>'users'));
 
 // Pass a storage object or array of storage objects to the OAuth2 server class
 $server = new OAuth2\Server($storage);
 
-// Add the "Client Credentials" grant type (it is the simplest of the grant types)
+
 $server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
 $server->addGrantType(new OAuth2\GrantType\UserCredentials($storage));
-// Add the "Authorization Code" grant type (this is where the oauth magic happens)
+
 $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
