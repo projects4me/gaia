b9c20411cad83ee87606a268b45b5e0e94820034
nouman <ranamnouman@yahoo.com>
2022-05-09 18:02:53 +0500

    Scoreboard module ~ controller, model, metadata ~ created

---
+++ app/metadata/model/Scoreboard.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+$models['Scoreboard'] = array(
+    'tableName' => 'scoreboard',
+    'fields' => array(
+        'id' => array(
+            'name' => 'id',
+            'label' => 'LBL_SCOREBOARD_ID',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false
+        ),
+        'userId' => array(
+            'name' => 'userId',
+            'label' => 'LBL_SCOREBOARD_USER_ID',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false
+        ),
+        'badgeId' => array(
+            'name' => 'badgeId',
+            'label' => 'LBL_SCOREBOARD_BADGE_ID',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false
+        ),
+        'score' => array(
+            'name' => 'score',
+            'label' => 'LBL_SCOREBOARD_SCORE',
+            'type' => 'int',
+            'length' => '100',
+            'null' => false
+        ),
+        'dateCreated' => array(
+            'name' => 'dateCreated',
+            'label' => 'LBL_SCOREBOARD_DATE_CREATED',
+            'type' => 'datetime',
+            'null' => true,
+        ),
+        'createdUser' => array(
+            'name' => 'createdUser',
+            'label' => 'LBL_SCOREBOARD_CREATED_USER',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false,
+        ),
+        'createdUserName' => array(
+            'name' => 'createdUserName',
+            'label' => 'LBL_SCOREBOARD_CREATED_USER_NAME',
+            'type' => 'varchar',
+            'length' => '50',
+            'null' => false,
+        ),
+        'dateModified' => array(
+            'name' => 'dateModified',
+            'label' => 'LBL_SCOREBOARD_DATE_MODIFIED',
+            'type' => 'datetime',
+            'null' => true,
+        ),
+        'modifiedUser' => array(
+            'name' => 'modifiedUser',
+            'label' => 'LBL_SCOREBOARD_MODIFIED_USER',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false,
+        ),
+        'modifiedUserName' => array(
+            'name' => 'modifiedUserName',
+            'label' => 'LBL_SCOREBOARD_MODIFIED_USER_NAME',
+            'type' => 'varchar',
+            'length' => '50',
+            'null' => false,
+        ),
+        'deleted' => array(
+            'name' => 'deleted',
+            'label' => 'LBL_SCOREBOARD_DELETED',
+            'type' => 'bool',
+            'length' => '1',
+            'null' => false,
+            'default' => 0,
+            'fts' => true
+        ),
+    ),
+    'indexes' => array(
+        'id' => 'primary',
+    ),
+    'foriegnKeys' => array(),
+    'triggers' => array(),
+    'relationships' => array(
+        'hasMany' => array(
+            'users' => array(
+                'primaryKey' => 'userId',
+                'relatedModel' => '\\Gaia\\MVC\\Models\\User',
+                'relatedKey' => 'id',
+            ),
+            'userBadges' => array(
+                'primaryKey' => 'badgeId',
+                'relatedModel' => '\\Gaia\\MVC\\Models\\Badgelevel',
+                'relatedKey' => 'badgeId',
+                'conditionExclusive' => 'Scoreboard.badgeId = userBadges.badgeId AND 
+                                         Scoreboard.score > userBadges.min_criteria AND
+                                         Scoreboard.score < userBadges.max_criteria'
+            )
+        )
+    ),
+    'behaviors' => array(
+        'auditBehavior',
+        'dateCreatedBehavior',
+        'dateModifiedBehavior',
+        'createdUserBehavior',
+        'modifiedUserBehavior',
+        'softDeleteBehavior'
+    )
+);
+
+return $models;
