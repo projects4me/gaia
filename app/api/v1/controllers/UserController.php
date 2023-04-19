2af51bd363f0e1826c3fe71673650eae2212ecce
Noman Naeem <ranamnouman@gmail.com>
2023-04-19 16:22:47 +0500

    Overriding `postAction()` metho of Rest Controller not required

--- app/api/v1/controllers/UserController.php
+++ app/api/v1/controllers/UserController.php
@@ -20,25 +20,4 @@ use Gaia\Libraries\Utils\Util;
  */
 class UserController extends RestController
 {
-    /**
-     * This function saves a user, which is done via the RestController. Before saving the
-     * user model, we're hashing the password of the user by using sha1() alogrithm. The
-     * reason to use sha1 is that our oauth library uses that algorithm to matches the password.
-     *
-     * @method postAction
-     * @return \Phalcon\Http\Response
-     */
-    public function postAction()
-    {
-        $util = new Util();
-        $requestData = $util->objectToArray($this->request->getJsonRawBody());
-
-        //hash password
-        $requestData['password'] = sha1($requestData['password']);
-
-        //pass data to postAction method of parent class
-        $response = parent::postAction($requestData);
-
-        return $response;
-    }
 }
