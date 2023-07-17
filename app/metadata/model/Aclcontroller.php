a26b253a928ace960796a38f052a84febbf3b144
Noman Naeem <ranamnouman@gmail.com>
2023-07-17 17:01:07 +0500

    Add `acl_controller` model

---
+++ app/metadata/model/Aclcontroller.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+$models['Aclcontroller'] = array(
+    'tableName' => 'acl_controllers',
+    'fields' => array(
+        'id' => array(
+            'name' => 'id',
+            'label' => 'LBL_ACL_CONTROLLERS_ID',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false,
+        ),
+        'entityId' => array(
+            'name' => 'entityId',
+            'label' => 'LBL_ACL_CONTROLLERS_ENTITY_ID',
+            'type' => 'varchar',
+            'length' => '50',
+            'null' => false,
+        ),
+        'entity' => array(
+            'name' => 'entity',
+            'label' => 'LBL_ACL_CONTROLLERS_ENTITY',
+            'type' => 'varchar',
+            'length' => '50',
+            'null' => false,
+        ),
+        'lft' => array(
+            'name' => 'lft',
+            'label' => 'LBL_ACL_CONTROLLERS_LEFT',
+            'type' => 'int',
+            'length' => '11',
+            'null' => false,
+        ),
+        'rht' => array(
+            'name' => 'rht',
+            'label' => 'LBL_ACL_CONTROLLERS_RIGHT',
+            'type' => 'int',
+            'length' => '11',
+            'null' => false,
+        ),
+        'parentId' => array(
+            'name' => 'parentId',
+            'label' => 'LBL_ACL_CONTROLLERS_PARENT_ID',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => true,
+        ),
+        'dateCreated' => array(
+            'name' => 'dateCreated',
+            'label' => 'LBL_ACL_CONTROLLERS_DATE_CREATED',
+            'type' => 'datetime',
+            'null' => true,
+        ),
+        'dateModified' => array(
+            'name' => 'dateModified',
+            'label' => 'LBL_ACL_CONTROLLERS_DATE_MODIFIED',
+            'type' => 'datetime',
+            'null' => true,
+        ),
+        'deleted' => array(
+            'name' => 'deleted',
+            'label' => 'LBL_ACL_CONTROLLERS_DELETED',
+            'type' => 'bool',
+            'length' => '1',
+            'null' => false,
+            'default' => 0
+        ),
+        'createdUser' => array(
+            'name' => 'createdUser',
+            'label' => 'LBL_ACL_CONTROLLERS_CREATED_USER',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false,
+        ),
+        'createdUserName' => array(
+            'name' => 'createdUserName',
+            'label' => 'LBL_ACL_CONTROLLERS_CREATED_USER_NAME',
+            'type' => 'varchar',
+            'length' => '50',
+            'null' => false,
+        ),
+        'modifiedUser' => array(
+            'name' => 'modifiedUser',
+            'label' => 'LBL_ACL_CONTROLLERS_MODIFIED_USER',
+            'type' => 'varchar',
+            'length' => '36',
+            'null' => false,
+        ),
+        'modifiedUserName' => array(
+            'name' => 'modifiedUserName',
+            'label' => 'LBL_ACL_CONTROLLERS_MODIFIED_USER_NAME',
+            'type' => 'varchar',
+            'length' => '50',
+            'null' => false,
+        )
+    ),
+    'indexes' => array(
+        'id' => 'primary',
+    ),
+    'foriegnKeys' => array(
+
+    ),
+    'triggers' => array(
+
+    ),
+    'functions' => array(),
+    'relationships' => array(),
+    'behaviors' => array(
+        'auditBehavior',
+        'dateCreatedBehavior',
+        'dateModifiedBehavior',
+        'createdUserBehavior',
+        'modifiedUserBehavior',
+        'softDeleteBehavior'
+    ),
+
+);
+
+return $models;
