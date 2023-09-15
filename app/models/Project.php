4b03dddfa4f2c3a3ee7cb8ccb5cdc88638ee2dc6
Noman Naeem <ranamnouman@gmail.com>
2023-09-15 18:15:51 +0500

    Update `project` model with added functions for ACL

--- app/models/Project.php
+++ app/models/Project.php
@@ -11,7 +11,7 @@ use Gaia\Core\MVC\Models\Model;
 /**
  * Project Model
  *
- * @author Hammad Hassan <gollomer@gmail.com>
+ * @author Rana Nouman <ranamnouman@gmail.com>
  * @package Foundation
  * @category Model
  * @license http://www.gnu.org/licenses/agpl.html AGPLv3
@@ -25,4 +25,76 @@ class Project extends Model
      * @var bool
      */
     public $splitQueries = false;
+
+    /**
+     * This function is used to apply ACL to the model.
+     * 
+     * @param \Phalcon\Mvc\Model $model
+     * @param string $userId
+     */
+    public static function applyACLByModel($model, $userId)
+    {
+        $di = \Phalcon\Di::getDefault();
+        $query = $model->getQuery();
+
+        $metadata = $di->get('metaManager')->getModelMeta((new self)->modelAlias);
+        $relatedKey = ($model instanceof \Gaia\MVC\Models\Project) ? 'id' : $metadata['acl']['group']['relatedKey'];
+
+        $groups = self::getGroups($userId, $metadata['acl']['group']);
+        $query->getPhalconQueryBuilder()->inWhere($model->modelAlias . ".$relatedKey", $groups);
+    }
+
+    /**
+     * This function is used to get the list of the groups on which user have membership.
+     * 
+     * @param string $userId
+     * @param array $aclMeta
+     * @return array
+     */    
+    private static function getGroups($userId, $aclMeta)
+    {
+        $groups = [];
+        $di = \Phalcon\Di::getDefault();
+        $queryBuilder = $di->get('modelsManager')->createBuilder();
+        $queryBuilder->columns($aclMeta['columns']);
+        $queryBuilder->from($aclMeta['relatedModel']);
+
+        $queryBuilder->where($aclMeta['condition'], [
+            'userId' => $userId
+        ]);
+
+        $memberships = $queryBuilder->getQuery()->execute();
+
+        foreach ($memberships as $membership) {
+            $groups[] = $membership['entityId'];
+        }
+
+        return $groups;
+    }
+
+    /**
+     * This function is used to apply acl on given relationship.
+     * 
+     * @param \Phalcon\Mvc\Model $model
+     * @param string $relName
+     * @param string $userId
+     */
+    public static function applyACLByRel($model, $relName, $userId)
+    {
+        $di = \Phalcon\Di::getDefault();
+
+        $metadata = $di->get('metaManager')->getModelMeta((new self)->modelAlias);
+        $relatedKey = $metadata['acl']['group']['relatedKey'];
+
+        $groups = self::getGroups($userId, $metadata['acl']['group']);
+
+        //prepare IN
+        $values = '';
+        foreach ($groups as $group) {
+            $values .= "$group,";
+        }
+
+        $values = substr($values, 0, -1);
+        $model->getRelationship()->addRelConditions($relName, "$relName.$relatedKey IN ('$values')");
+    }
 }
