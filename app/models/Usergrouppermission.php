2eb0008905111e716f1e6d810df1336f8463c685
Noman Naeem <ranamnouman@gmail.com>
2023-10-04 17:40:06 +0500

    Create model for both views

---
+++ app/models/Usergrouppermission.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+namespace Gaia\MVC\Models;
+
+use Gaia\Core\MVC\Models\Model;
+
+/**
+ * Acl Permissions of User on different resources against a group e.g project.
+ *
+ * @author Rana Nouman <ranamnouman@gmail.com>
+ * @package Foundation
+ * @category Model
+ * @license http://www.gnu.org/licenses/agpl.html AGPLv3
+ */
+class Usergrouppermission extends Model
+{
+
+}
