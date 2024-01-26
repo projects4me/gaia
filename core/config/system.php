dee50e26ded937f31461f5f610f2b7eb00625ac6
Noman Naeem <ranamnouman@gmail.com>
2024-01-26 11:16:15 +0500

    Add `system` config file

---
+++ core/config/system.php
@@ -0,0 +1,23 @@
+<?php
+
+// Get permission flags
+$filePath = APP_PATH . '/app/metadata/model/Permission.php';
+$fields = array_keys(($this->di->get('fileHandler')->readFile($filePath))['Permission']['fields']);
+$permissionFlags = array_values(array_filter($fields, function ($field) {
+    return strpos($field, 'F') !== false;
+}));
+
+// This contains the system settings
+$config['system'] = [
+    'acl' => [
+            'permissionFlags' => $permissionFlags,
+            'apiOptions' => [
+                'all' => '9',
+                'group' => '2',
+                'assignment' => '1',
+                'none' => '0'
+            ]
+        ]
+    ];
+
+return $config;
