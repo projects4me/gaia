91a8e58f0da6cc7ffff2e4a9df2132281ab3878a
Noman Naeem <ranamnouman@gmail.com>
2024-02-19 21:11:22 +0500

    Retrieve model groups and add it to configurations

--- core/config/system.php
+++ core/config/system.php
@@ -11,16 +11,39 @@ $permissionFlags = array_values(
     )
 );
 
+// Get groups for each model.
+$models = $this->di->get('fileHandler')->readFile(APP_PATH . '/core/config/models.php');
+$modelGroups = [];
+
+// By now settings are not set as global variable. So we're just trying to populate the models
+// array that will be used inside the metaManager getGroups() method.
+global $settings;
+$settings['models'] = $models['models'];
+
+foreach ($models['models'] as $modelName) {
+    $modelGroups[$modelName] = $this->di->get('metaManager')->getModelGroups($modelName);
+}
+
 // This contains the system settings.
 $config['system'] = [
     'acl' => [
             'permissionFlags' => $permissionFlags,
             'apiOptions' => [
-                'all' => '9',
-                'group' => '2',
-                'assignment' => '1',
-                'none' => '0'
-            ]
+                'field' => [
+                    'allow' => '9',
+                    'none' => '0'
+                ],
+                'model' => [
+                    'all' => '9',
+                    'assignment' => '1',
+                    'none' => '0'
+                ],
+                'groups' => [
+                    'project' => '2',
+                    'conversation' => '3'
+                ]
+            ],
+            'modelGroups' => $modelGroups
         ]
     ];
 
