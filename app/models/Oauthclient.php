92a7f5f5c415152dc734bf86c435c1b9a6590b6f
Noman Naeem <ranamnouman@gmail.com>
2024-03-26 13:30:34 +0500

    Apply acl on required models

--- app/models/Oauthclient.php
+++ app/models/Oauthclient.php
@@ -11,11 +11,17 @@ use Gaia\Core\MVC\Models\Model;
 /**
  * Oauthclient Model
  *
- * @author Hammad Hassan <gollomer@gmail.com>
- * @package Foundation
+ * @author   Hammad Hassan <gollomer@gmail.com>
+ * @package  Foundation
  * @category Model
- * @license http://www.gnu.org/licenses/agpl.html AGPLv3
- */class Oauthclient extends Model
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
+ */
+class Oauthclient extends Model
 {
-    
+    /**
+     * Flag to determine if ACL rules should be applied to the model.
+     *
+     * @var bool
+     */
+    protected $aclAllowed = false;
 }
--- app/models/Oauthjwt.php
+++ app/models/Oauthjwt.php
@@ -11,12 +11,17 @@ use Gaia\Core\MVC\Models\Model;
 /**
  * Oauthjwt Model
  *
- * @author Hammad Hassan <gollomer@gmail.com>
- * @package Foundation
+ * @author   Hammad Hassan <gollomer@gmail.com>
+ * @package  Foundation
  * @category Model
- * @license http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
  */
 class Oauthjwt extends Model
 {
-    
+    /**
+     * Flag to determine if ACL rules should be applied to the model.
+     *
+     * @var bool
+     */
+    protected $aclAllowed = false;
 }
