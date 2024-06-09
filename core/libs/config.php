7d92bbbc9b650aae4b2b151cc0ea3bf55e107206
Noman Naeem <ranamnouman@gmail.com>
2024-06-09 21:09:05 +0500

    Add config into the `config` class property and use get method to access those configs

--- core/libs/config.php
+++ core/libs/config.php
@@ -13,17 +13,16 @@ use Phalcon\Config as PhalconConfig;
  * configurations in the application. It uses Phalcon\Config and is responsible
  * for reading all the default and custom configurations and exposing them
  * through a global variable.
- * 
+ *
  * @param array $config this configuration in the system
- * 
- * @author Hammad Hassan <gollomer@gmail.com>
- * @package Foundation
+ *
+ * @author   Hammad Hassan <gollomer@gmail.com>
+ * @package  Foundation
  * @category Config
- * @license http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
  */
 class Config
 {
-
     /**
      * This is the dependency injector used by this class
      *
@@ -48,55 +47,69 @@ class Config
      * @var $config
      */
     protected $config;
-    
+
     /**
      * This function is responsible for initializing the configurations and
      * populating them in the glocal variable $config.
-     * 
+     *
      * @todo make sure that the merge allows config to be overwritten as well
      */
     public function init()
     {
-       
+
         global $settings;
         /* First load all the default configurations        */
         $default = $this->loadDefault();
-        
+
         /* Next load all the custom configurations        */
         $custom = $this->loadCustom();
-        
+
         // Initiate the routes
         $settings = new PhalconConfig($default);
         $customSettings = new PhalconConfig($custom);
-        
+
         // Merge them
         $settings->merge($customSettings);
+
+        $this->config = $settings;
     }
-    
+
     /**
-     * This function is responsible including the necassary default 
+     * This function is responsible including the necassary default
      * configurations
-     * 
+     *
      * @return array default configurations
-     * @todo merge all the files in folder to one files in future
+     * @todo   merge all the files in folder to one files in future
      */
-    public function loadDefault(){
+    public function loadDefault()
+    {
         // Loading all the files in core/config/
         return $this->di->get('fileHandler')->readFolder(APP_PATH.'/core/config/');
     }
-    
+
     /**
-     * This function is responsible including the necassary custom 
+     * This function is responsible including the necassary custom
      * configurations, if any
-     * 
+     *
      * @return array custom configurations
-     * @todo merge all the files in folder to one files in future
+     * @todo   merge all the files in folder to one files in future
      */
-    public function loadCustom(){
+    public function loadCustom()
+    {
         // Loading all the files in config/
         return $this->di->get('fileHandler')->readFolder(APP_PATH.'/config/');
     }
-    
 
+    /**
+     * This function is used to get the configuration against the given key.
+     *
+     * @method get
+     * @return \Phalcon\Config|null
+     */
+    public function get($key)
+    {
+        $config = $this->config[$key] ?? null;
 
+        return $config;
+    }
 }
