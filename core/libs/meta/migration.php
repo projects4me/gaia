e7951d6728822e2c8365690d5f650177b6bf1307
Rana Nouman <77791428+rana-nouman@users.noreply.github.com>
2026-01-29 13:27:56 +0500

    Add collation support in migration (#147)

--- core/libs/meta/migration.php
+++ core/libs/meta/migration.php
@@ -25,10 +25,10 @@ use Gaia\Libraries\Meta\Manager as metaManager;
  *
  * This calss is dependant on Phalcon Dev Tools.
  *
- * @author Hammad Hassan <gollomer@gmail.com>
- * @package Foundation\Mvc\Model
+ * @author   Hammad Hassan <gollomer@gmail.com>
+ * @package  Foundation\Mvc\Model
  * @category Migration
- * @license http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
  */
 class Migration extends PhalconMigration
 {
@@ -58,7 +58,7 @@ class Migration extends PhalconMigration
      * app\metadata\model it would be redundant to have the columns defined
      * again so this function get the metadata from the files and sets them up
      *
-     * @todo get table engines and collation from the model meta and system config
+     * @todo  get table engines and collation from the model meta and system config
      * @param $model
      */
     public function up($model)
@@ -116,9 +116,9 @@ class Migration extends PhalconMigration
      * This function is responsible for migrating triggers, attached to a specific
      * table. It checks whether trigger exist or not, if it not exists than it will
      * create a new one.
-     * 
+     *
      * @param $model
-     * @param $meta 
+     * @param $meta
      */
     private function migrateTriggers($model, $meta)
     {
@@ -167,8 +167,8 @@ class Migration extends PhalconMigration
      * This function is responsible for reading the individual metadata from
      * the application and process it for this class
      *
-     * @todo Include parsing for all type of indexes
-     * @param string $model
+     * @todo   Include parsing for all type of indexes
+     * @param  string $model
      * @return array
      */
     private function prepareTableDefinition($model, $meta)
@@ -184,8 +184,9 @@ class Migration extends PhalconMigration
         foreach ($meta[$model]['fields'] as $field => $schema) {
             $fieldOptions = array();
             $fieldOptions['type'] = $this->di->get('metaManager')->getFieldType($schema['type']);
-            if (isset($schema['length']))
+            if (isset($schema['length'])) {
                 $fieldOptions['size'] = $schema['length'];
+            }
             $fieldOptions['notNull'] = !$schema['null'];
             if (isset($schema['autoIncrement'])) {
                 $fieldOptions['autoIncrement'] = $schema['autoIncrement'];
@@ -193,6 +194,15 @@ class Migration extends PhalconMigration
 
             if (isset($schema['default'])) {
                 $fieldOptions['default'] = $schema['default'];
+            }
+
+            // Add charset and collation if both are present in metadata
+            if ($this->shouldApplyCollation($schema['type'])
+                && isset($schema['charset'])
+                && isset($schema['collation'])
+            ) {
+                $fieldOptions['charset'] = $schema['charset'];
+                $fieldOptions['collation'] = $schema['collation'];
             }
 
             $tableDescription['columns'][] = new Column($schema['name'], $fieldOptions);
@@ -223,12 +233,25 @@ class Migration extends PhalconMigration
     /**
      * This function returns metadata of given model.
      *
-     * @param $model
+     * @param  $model
      * @return array
      */
     private function getMetaData($model)
     {
         return $this->di->get('fileHandler')->readFile(APP_PATH . metaManager::basePath . '/model/' . $model . '.php');
+    }
+
+    /**
+     * This function determines if a field type should support charset/collation.
+     * Only text-based column types support charset/collation.
+     *
+     * @param  string $fieldType
+     * @return bool
+     */
+    private function shouldApplyCollation($fieldType)
+    {
+        $textTypes = array('varchar', 'char', 'text', 'tinytext', 'mediumtext', 'longtext');
+        return in_array(strtolower($fieldType), $textTypes);
     }
 
     /**
@@ -241,5 +264,106 @@ class Migration extends PhalconMigration
     {
         $migration = new Migration($this->di);
         $migration->up($model);
+    }
+
+    /**
+     * This function is responsible for migrating column charset and collation
+     * for existing tables. It compares the metadata with the current database
+     * state and updates columns that have charset/collation defined in metadata.
+     *
+     * @param string $model
+     */
+    public function migrateColumnCollation($model)
+    {
+        $meta = $this->getMetaData($model);
+        
+        // Skip if this is a view
+        if (isset($meta[$model]['isView'])) {
+            return;
+        }
+
+        $tableName = $meta[$model]['tableName'];
+        $dialect = $this->di->get('dialect');
+        $metaManager = $this->di->get('metaManager');
+
+        // Process each field that has charset and collation defined
+        foreach ($meta[$model]['fields'] as $field => $schema) {
+            if (!isset($schema['charset']) || !isset($schema['collation'])) {
+                continue;
+            }
+
+            if (!$this->shouldApplyCollation($schema['type'])) {
+                continue;
+            }
+
+            $columnName = $schema['name'];
+            $expectedCharset = $schema['charset'];
+            $expectedCollation = $schema['collation'];
+
+            $collationQuery = $dialect->getColumnCollation($tableName, $columnName);
+            $result = $this::$connection->query($collationQuery)->fetch();
+
+            if (empty($result) || empty($result['COLLATION_NAME'])) {
+                continue;
+            }
+
+            $currentCollation = $result['COLLATION_NAME'];
+            $currentCharset = $result['CHARACTER_SET_NAME'];
+
+            if ($currentCollation !== $expectedCollation
+                || $currentCharset !== $expectedCharset
+            ) {
+                $columnType = $this->getColumnTypeString($schema, $metaManager);
+                $columnDefinition = $columnType;
+                
+                if (isset($schema['length'])) {
+                    $columnDefinition .= '(' . $schema['length'] . ')';
+                }
+
+                $alterQuery = $dialect->alterColumnCollation(
+                    $tableName,
+                    $columnName,
+                    $columnDefinition,
+                    $expectedCharset,
+                    $expectedCollation
+                );
+
+                $this::$connection->execute($alterQuery);
+            }
+        }
+    }
+
+    /**
+     * This function converts metadata field type to SQL column type string.
+     *
+     * @param  array                        $schema
+     * @param  \Gaia\Libraries\Meta\Manager $metaManager
+     * @return string
+     */
+    private function getColumnTypeString($schema, $metaManager)
+    {
+        $type = strtolower($schema['type']);
+        
+        $typeMap = array(
+            'varchar' => 'VARCHAR',
+            'char' => 'CHAR',
+            'text' => 'TEXT',
+            'tinytext' => 'TINYTEXT',
+            'mediumtext' => 'MEDIUMTEXT',
+            'longtext' => 'LONGTEXT',
+            'int' => 'INT',
+            'bigint' => 'BIGINT',
+            'float' => 'FLOAT',
+            'decimal' => 'DECIMAL',
+            'datetime' => 'DATETIME',
+            'date' => 'DATE',
+            'bool' => 'TINYINT',
+            'blob' => 'BLOB',
+            'tinyblob' => 'TINYBLOB',
+            'mediumblob' => 'MEDIUMBLOB',
+            'longblob' => 'LONGBLOB'
+        );
+
+        return isset($typeMap[$type]) ? $typeMap[$type] : strtoupper($type);
     }
 }
