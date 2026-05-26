ad33b0bf4bbaf3243a0ead28332cec8807401817
Rana Nouman <77791428+rana-nouman@users.noreply.github.com>
2026-05-26 18:30:37 +0500

    Add user skill and qualification model (#159)

---
+++ app/models/Userqualification.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+namespace Gaia\MVC\Models;
+
+use Gaia\Core\MVC\Models\Model;
+
+/**
+ * User Qualification Model
+ *
+ * @author Hammad Hassan <gollomer@gmail.com>
+ * @package Foundation
+ * @category Model
+ * @license http://www.gnu.org/licenses/agpl.html AGPLv3
+ */
+class Userqualification extends Model
+{
+
+}
